e5fab10d4b9599df873372e76121aa5a676c4bc6
Piyush Panchal <piyushpanchal114@gmail.com>
2025-05-26 11:08:14 +0530

    admin setup

--- knox/core/admin.py
+++ knox/core/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Plan, Subscription, Invoice
 
-# Register your models here.
+
+@admin.register(Plan)
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'billing_cycle_days', 'is_active')
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('user', 'plan', 'status', 'start_date', 'end_date')
+
+
+@admin.register(Invoice)
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('subscription', 'user', 'plan', 'amount', 'issue_date',
+                    'due_date', 'paid_date', 'status')
